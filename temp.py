7d1a27634d0a092f6c67cd0090cb83851dea0f50
Mehedi-Hasan-NSL <86214527+Mehedi-Hasan-NSL@users.noreply.github.com>
2021-06-21 10:27:34 +0600

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
