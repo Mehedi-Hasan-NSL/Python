34a5f05ce13fde4b032a2e09d2ad6098221b33fa
Mehedi-Hasan-NSL <86214527+Mehedi-Hasan-NSL@users.noreply.github.com>
2021-06-21 16:32:59 +0600

    Add files via upload

---
+++ re.split().py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 21 16:05:05 2021
+
+@author: DELL
+"""
+
+regex_pattern = r""	# Do not delete 'r'.
+
+import re
+print("\n".join(re.split(regex_pattern, input())))
