0edf3e78a1283fe7209a9f9373d1b36f1e58f950
Mehedi-Hasan-NSL <86214527+Mehedi-Hasan-NSL@users.noreply.github.com>
2021-06-24 17:11:46 +0600

    Add files via upload

---
+++ triangle_quest2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 24 17:11:04 2021
+
+@author: DELL
+"""
+
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not                                         leave a blank line also
+    print (((10**(i-1)+(10**(i-1)//9))**2))
